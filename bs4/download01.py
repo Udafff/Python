5a6440d5b8c39d51d13495cf66b13551b6ec26fc
Artyom Udovichenko <aptem-1986@yandex.ru>
2018-12-30 12:18:08 +0300

    Python bs4 Project init

---
+++ bs4/download01.py
@@ -0,0 +1,17 @@
+""" Download class. """
+
+import requests
+
+
+class DownloadFile:
+    def __init__(self, arg_url):
+        self.url = arg_url
+
+    def habr_example(self):
+        url = self.url
+
+
+if __name__ == "__main__":
+    print('Hello Gold')
+    habr_test = DownloadFile()
+
